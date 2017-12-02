378d6603f631c044faf23e1d4e2c563fcb6d8bad
Luiz Neiva <luizgzn@gmail.com>
2017-12-02 01:27:58 -0200

    Added a JS to move the navbar

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,16 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+window.onscroll = (e) => {
+  let scrollPosition = window.scrollY
+  let headerName = document.getElementById("name")
+  let header = document.getElementById("navbar-wagon")
+  if (scrollPosition > 20) {
+    headerName.classList.remove("hidden")
+    header.classList.add("white")
+  } else {
+    headerName.classList.add("hidden")
+    header.classList.remove("white")
+  }
+}
