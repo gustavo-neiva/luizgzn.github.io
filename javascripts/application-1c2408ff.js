3de0692afcbf636239163dd56e8957dca9632c87
Luiz Neiva <luizgzn@gmail.com>
2017-12-05 16:17:52 -0200

    Automated commit at 2017-12-05 18:17:52 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/application-1c2408ff.js
@@ -0,0 +1,15 @@
+
+
+// Header movement //
+
+window.onscroll = (e) => {
+  let scrollPosition = window.scrollY;
+  let headerName = document.getElementById("name");
+  let header = document.getElementById("header");
+  if (scrollPosition > 21) {
+    header.classList.add("opacity")
+  } else {
+    header.classList.remove("opacity")
+  }
+}
+;
